73d8c61f8660c229f67c4fcc6f8fa9d6ec0b4683
Berny <bernyblog@gmail.com>
2022-01-14 22:28:23 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10;
+let dcr  = 10;
+
+// ++incr;
+// --dcr;
+
+console.log(++incr);
+console.log(--dcr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = true,
+      isClose   = false;
+
+console.log(isChecked || !isClose);
+
